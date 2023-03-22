662038119af80f875aff2de8b785ddefcbd040f5
Bruno C. Martins <101012137+brunocmartins11@users.noreply.github.com>
2023-03-22 10:50:42 -0300

    Mudança de Lógica
    Nesse commit, venho mudar a lógica por trás do conversor kg/peças, assim deixando o mais completo em casos do usuário não querer inserir os kg de material comprado.

--- disco.js
+++ disco.js
@@ -49,27 +49,39 @@ function disco() {
 
         var total = totalDisco - totalFuro;
 
-        
+
 
     } else {
         var raio = diametro / 2;
         var volume = 3.14 * (raio * raio) * altura;
         var total = volume * pesoEsp;
-        
+
     }
 
-        var conversao = total / 1000000;
-        var conversao2 = total / 1000;
-    
-    for(let i = 0; i < 1; i++){
-            var kg = prompt("Insira o peso comprado")
-        
-            var pecas = Number(kg) / conversao
-        }
+    var conversao = total / 1000000;
+    var conversao2 = total / 1000;
+
+    if (confirm("Deseja converter o peso do material comprado para peças totais?") == true) {
+
+            var material = prompt("Insira o peso comprado")
+            
+            var pecas = Number(material) / conversao
+
+            document.getElementById("kg").innerText = `${conversao} kg`
+            document.getElementById("g").innerText = `${conversao2} g`
+            document.getElementById("materiais").innerText = `${material} kg de material`
+            document.getElementById("pecas").innerText = `${pecas.toFixed(2)} peças totais`
+
+    }
+    else {
+        alert("Você cancelou a operação!")
+
+        document.getElementById("kg").innerText = `${conversao} kg`
+        document.getElementById("g").innerText = `${conversao2} g`
+    }
+
 
     window.scrollTo(0, 500);
 
-    document.getElementById("kg").innerText = `${conversao} kg`
-    document.getElementById("g").innerText = `${conversao2} g`
-    document.getElementById("pecas").innerText = `${pecas.toFixed(1)} peças totais`
+
 }
--- retangulo.js
+++ retangulo.js
@@ -8,37 +8,46 @@ function retangulo() {
     Number(largura)
     Number(altura)
     Number(comprimento)
-    
-switch (material) {
 
-case "1":
-pesoEsp = 2.7;
-break;
-case "2":
-pesoEsp = 8.0;
-break;
-case "3":
-pesoEsp = 8.9;
-break;
-case "4":
-pesoEsp = 8.8;
-break;
-}
+    switch (material) {
 
-var peso = largura * comprimento * altura * pesoEsp
-var conversao = peso / 1000000;
-var conversao2 = peso / 1000;
+        case "1":
+            pesoEsp = 2.7;
+            break;
+        case "2":
+            pesoEsp = 8.0;
+            break;
+        case "3":
+            pesoEsp = 8.9;
+            break;
+        case "4":
+            pesoEsp = 8.8;
+            break;
+    }
 
-for(let i = 0; i < 1; i++){
-    var kg = prompt("Insira o peso comprado")
+    var peso = largura * comprimento * altura * pesoEsp
+    var conversao = peso / 1000000;
+    var conversao2 = peso / 1000;
 
-    var pecas = Number(kg) / conversao
-}
+    if (confirm("Deseja converter o peso do material comprado para peças totais?") == true) {
 
-window.scrollTo(0, 500);
+        var material = prompt("Insira o peso comprado")
 
-document.getElementById("kg").innerText = `${conversao} kg`
-document.getElementById("g").innerText = `${conversao2} g`
-document.getElementById("pecas").innerText = `${pecas.toFixed(1)} peças totais`
+        var pecas = Number(material) / conversao
+
+        document.getElementById("kg").innerText = `${conversao} kg`
+        document.getElementById("g").innerText = `${conversao2} g`
+        document.getElementById("materiais").innerText = `${material} kg de material`
+        document.getElementById("pecas").innerText = `${pecas.toFixed(2)} peças totais`
+
+    }
+    else {
+        alert("Você cancelou a operação!")
+
+        document.getElementById("kg").innerText = `${conversao} kg`
+        document.getElementById("g").innerText = `${conversao2} g`
+    }
+
+    window.scrollTo(0, 500);
 
 }
